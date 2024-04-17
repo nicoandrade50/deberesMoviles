fee14debbef3cac9f3820fab84ce7a241dc64e23
nicoandrade50 <83848730+nicoandrade50@users.noreply.github.com>
2024-04-16 20:35:00 -0500

    events

--- semana 2/tc/chocolate-factory-mobile-02-Comunicaci-n-de-Componentes-y-Ciclo-de-Vida/src/components/EventCommunication/WillyWonka.tsx
+++ semana 2/tc/chocolate-factory-mobile-02-Comunicaci-n-de-Componentes-y-Ciclo-de-Vida/src/components/EventCommunication/WillyWonka.tsx
@@ -5,23 +5,35 @@ import './WillyWonka.css';
 // WillyWonka is the parent component that passes the event handler to the OompaLoompa component via props
 export const WillyWonkaEvent: FC = () => {
     const [orders, setOrders] = useState<string>("Make more chocolates🍫🍫🍫!");
+    const [childMessage, setChildMessage] = useState<string>("");
 
     const changeOrders = (): void => {
         setOrders("Make more candies 🍭🍭🍭!");
     };
+    const handleChildMessage = (message: string): void => {
+        
+    
+       // console.log("Message from child:", message);
+       setChildMessage(message);
+    };
 
-    // let orders = "Make more chocolates 🍫🍫🍫!";
-    //
-    // const changeOrders = (): void => {
-    //     orders = "Make more candies 🍭🍭🍭!";
-    // };
+
+
+/*
+     const changeOrders = (): void => {
+         orders = "Make more candies 🍭🍭🍭!";
+     }; */
+
+
+
 
     return (
         <div className='background'>
             <h1>Willy Wonka 🔊:</h1>
-            <OompaLoompaEvent orders={orders} onOrderChange={changeOrders}/>
+            <OompaLoompaEvent orders={orders} onOrderChange={changeOrders} onChildMessage={handleChildMessage}/>
+            <h1>Child Message: {childMessage}</h1>
             {/*<OompaLoompaEvent orders={orders} onOrderChange={() => setOrders("Make more candies 🍭🍭🍭!")}/>*/}
         </div>
     );
 }
-
+     let orders = "Make more chocolates 🍫🍫🍫!";
